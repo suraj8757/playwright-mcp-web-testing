551bc0ebde6cec3ea777aeece9467b4e885aacfc
suraj8757 <85273070+suraj8757@users.noreply.github.com>
2026-04-28 22:30:37 +0530

    Create tests/clientWork.spec.ts

---
+++ tests/clientWork.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('EPAM Client Work navigation', () => {
+  test('navigates from Services to Client Work and verifies text', async ({ page }) => {
+    // TEST_BODY
+  });
+});
